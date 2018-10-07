8a1cc6f7c3d1a672b127dade97a39e96444a98b9
Mohit Khattar <mk1483@pwd.cs.rutgers.edu>
2018-10-06 22:55:31 -0400

    Added a unified interface for map() and cleaned up the input parsing

--- mapreduce/mapred.h
+++ mapreduce/mapred.h
@@ -13,6 +13,21 @@ typedef struct _TpTable {
   struct _TpTable *next;
 } TpTable;
 
+
+/*
+ * Wrapper function for both the map_processes() and map_threads()
+ * functions.
+ * First, parse through the input file. Then, build a TpTable array
+ * based on the number of maps we need to do. Then, send those off
+ * to either processes (if processes == 1) or threads (if 
+ * processes == 0).
+ * Return the result of the mapping operation.
+ */
+LinkedList *map(char *filename, int processes, int num_maps);
+
+
+
+
 void *map_thread_handler(void *args);
 
 LinkedList *tpTable_to_list(TpTable *table);
@@ -23,7 +38,7 @@ LinkedList *tpTable_to_list(TpTable *table);
  */
 LinkedList *map_threads(TpTable **hashmap, int num_maps);
 
-LinkedList * map(TpTable ** hashmap, LinkedList * list, int num_maps, int array_size, int * map_size);
+LinkedList *map_processes(TpTable ** hashmap, LinkedList * list, int num_maps, int array_size, int * map_size);
 
 void process_map(int start_index, int end_index, char * sharedmemory);
 
